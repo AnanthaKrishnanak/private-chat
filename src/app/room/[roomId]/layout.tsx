1b18f85b309c00cb4d9ac4267d8838889fe58eea
Anantha Krishnan <ananthakrishnan.s@scrapehero.com>
2025-12-08 21:52:36 +0530

    feat: room layout

---
+++ src/app/room/[roomId]/layout.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { useParams } from "next/navigation";
+import React, { useState } from "react";
+import { Copy, Check } from "lucide-react";
+import { cn } from "@/lib/cn";
+
+function formatTimeRemaining(timeRemaining: number) {
+  const minutes = Math.floor(timeRemaining / 60);
+  const seconds = timeRemaining % 60;
+  return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+}
+
+function Layout({ children }: { children: React.ReactNode }) {
+  const { roomId } = useParams();
+  const [copied, setCopied] = useState(false);
+  const timeRemaining = 10;
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(window.location.href);
+      setCopied(true);
+
+      setTimeout(() => setCopied(false), 1200);
+    } catch (err) {
+      console.error("Failed to copy:", err);
+    }
+  };
+
+  return (
+    <main className="h-screen flex flex-col max-h-screen overflow-hidden">
+      <header className="border-b border-zinc-800 p-4 flex items-center justify-between bg-zinc-900/30">
+        <div className="flex gap-4">
+          <div className="flex flex-col">
+            <h3 className="text-xs text-zinc-500">ROOM ID:</h3>
+
+            <div className="flex items-center gap-2">
+              <p className="text-green-500">{roomId}</p>
+
+              <button
+                onClick={handleCopy}
+                className="text-zinc-400 hover:text-white transition-colors"
+                aria-label="Copy room URL"
+              >
+                {copied ? <Check size={16} /> : <Copy size={16} />}
+              </button>
+            </div>
+          </div>
+          <div className="h-8 w-px bg-zinc-800" />
+          <div className="flex flex-col">
+            <span className="text-xs text-zinc-500 uppercase">
+              SELF- DESTRUCT:
+            </span>
+            <span
+              className={cn(
+                "text-sm font-bold flex items-center gap-2",
+                timeRemaining !== null && timeRemaining < 60
+                  ? "text-red-500"
+                  : "text-amber-500"
+              )}
+            >
+              {timeRemaining ? formatTimeRemaining(timeRemaining) : "--:--:--"}
+            </span>
+          </div>
+        </div>
+        <button className="text-zinc-400 bg-zinc-800 hover:text-white  hover:bg-red-700 px-3 py-1.5 transition-all uppercase ">
+          Destroy Now
+        </button>
+      </header>
+
+      <div className="flex-1 overflow-y-scroll p-4 space-y-4 scrollbar-thin">
+        {children}
+      </div>
+    </main>
+  );
+}
+
+export default Layout;
--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+"use client";
+
+import { useParams } from "next/navigation";
+import { Send } from "lucide-react";
+import { useRef, useState } from "react";
 
 const Room = () => {
-  return <div>Room</div>;
+  const { roomId } = useParams();
+  const [message, setMessage] = useState("");
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSend = () => {
+    if (!message.trim()) return;
+    console.log("Send message:", message);
+    setMessage("");
+    inputRef.current?.focus();
+  };
+
+  return (
+    <div className="flex flex-col flex-1 h-full">
+      <div className="flex-1 p-4">
+        <h1 className="text-2xl font-bold">Room {roomId}</h1>
+      </div>
+
+      <div className="p-4 border-t border-zinc-800 bg-zinc-900/40">
+        <div className="flex items-center gap-3">
+          <input
+            value={message}
+            autoFocus
+            type="text"
+            onChange={(e) => setMessage(e.target.value)}
+            onKeyDown={(e) =>
+              e.key === "Enter" && message.trim() && handleSend()
+            }
+            className="flex-1 bg-zinc-800 text-white placeholder-zinc-500 
+                       px-4 py-3 rounded-xl border border-zinc-700 
+                       focus:outline-none focus:ring-2 focus:ring-green-600/40"
+            placeholder="Type a message..."
+          />
+
+          <button
+            onClick={handleSend}
+            className="p-3 rounded-xl bg-green-600 hover:bg-green-500 transition 
+                       text-white flex items-center justify-center"
+            aria-label="Send Message"
+          >
+            <Send size={18} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Room;
